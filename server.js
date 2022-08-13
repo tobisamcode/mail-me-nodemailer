13b7ce1a3740b80472f558edc51670c1103bfc2b
tobisamcode <samueloluwatobiloba48@gmail.com>
2022-08-13 23:44:30 +0100

    nodemailer

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const ejs = require("ejs");
 const app = express();
+const nodemailer = require("nodemailer");
 
 const PORT = process.eventNames.PORT || 5000;
 
@@ -17,6 +18,32 @@ app.get("/", (req, res) => {
 
 app.post("/", (req, res) => {
   console.log(req.body);
+
+  const transporter = nodemailer.createTransport({
+    service: "Gmail",
+    auth: {
+      user: "samueloluwatobiloba48@gmail.com",
+      pass: "nelson2000"
+    }
+  });
+
+  const mailOPtions = {
+    from: req.body.email,
+    to: "samueloluwatobiloba48@gmail.com",
+    subject: `Message from ${req.body.name}, ${req.body.email}: ${req.body
+      .subject}`,
+    text: req.body.message
+  };
+
+  transporter.sendMail(mailOPtions, (error, info) => {
+    if (error) {
+      console.log(error);
+      res.send("error");
+    } else {
+      console.log("Email sent: " + info.response);
+      res.send("success");
+    }
+  });
 });
 
 app.listen(PORT, () => {
